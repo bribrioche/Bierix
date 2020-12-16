16663784e8722ffff5605ecb7c80f6c931c9ea6c
APB-s <a.saillour@protonmail.com>
2020-12-16 01:29:40 +0100

    Implémentation changement d'activité

--- app/src/main/java/fr/brioche/bierix/login_class.java
+++ app/src/main/java/fr/brioche/bierix/login_class.java
@@ -1,4 +1,40 @@
 package fr.brioche.bierix;
 
-public class login_class {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class login_class extends AppCompatActivity
+{
+
+    private Button Connexion;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        Connexion = (Button) findViewById(R.id.Connexion);
+        Connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
