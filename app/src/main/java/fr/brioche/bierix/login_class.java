3bf7cdd75243eaeb511703b297088da6a808df26
bryan <bryan.guillot@outlook.com>
2020-12-16 21:49:00 +0100

    underscores

--- app/src/main/java/fr/brioche/bierix/login_class.java
+++ app/src/main/java/fr/brioche/bierix/login_class.java
@@ -12,23 +12,26 @@ import android.widget.Button;
 public class login_class extends AppCompatActivity
 {
 
-    private Button Connexion;
-
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
-        Connexion = (Button) findViewById(R.id.connexion_bttn);
-        Connexion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                openMainActivity();
-            }
-        });
     }
+
+    //private Button Connexion;
+
+    //@SuppressLint("WrongViewCast")
+   // @Override
+   // protected void onCreate(Bundle savedInstanceState) {
+   //     super.onCreate(savedInstanceState);
+//
+    //    Connexion = (Button) findViewById(R.id.connexion_bttn);
+    //    Connexion.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view)
+     //       {
+      //          openMainActivity();
+//        });
+   // }
 
     public void openMainActivity()
     {
