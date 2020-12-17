cbdd37ee420610db1c50e707008613e666081516
Nasreb <n.berro72@gmail.com>
2020-12-17 06:48:57 +0100

    Panier

---
+++ app/src/main/java/fr/brioche/bierix/Panier.java
@@ -0,0 +1,45 @@
+package fr.brioche.bierix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class Panier extends AppCompatActivity {
+
+    private ImageButton btnUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panier);
+
+        initActivity();
+
+    }
+
+    private void initActivity(){
+        btnUser = (ImageButton)findViewById(R.id.btnUser);
+
+        createOnClicBtnUser();
+
+    }
+
+    private void createOnClicBtnUser(){
+        btnUser.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityLogin();
+            }
+        });
+    }
+
+    public void openActivityLogin(){
+        Intent intent = new Intent(Panier.this, login_class.class);
+        startActivity(intent);
+    }
+}
