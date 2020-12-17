cf6a13fef1f8cf6b570abc5ad110ae25a99f1cac
APB-s <a.saillour@protonmail.com>
2020-12-17 19:56:58 +0100

    ajout des queqlues trucs sur description

--- app/src/main/java/fr/brioche/bierix/Redirection.java
+++ app/src/main/java/fr/brioche/bierix/Redirection.java
@@ -32,6 +32,6 @@ public class Redirection extends AppCompatActivity {
                     Intent intent = new Intent(Redirection.this, MainActivity.class);
                     startActivity(intent);
                 }
-        }, 3000);
+        }, 2000);
     }
 }
